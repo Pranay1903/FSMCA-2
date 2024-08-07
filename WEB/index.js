de66bf9e3c0251d639f5d66ed0341e64d0254483
Pranay Patel <pranaypatel1933@gmail.com>
2024-08-07 23:58:01 +0530

    Explored Datatypes in JS

---
+++ WEB/index.js
@@ -0,0 +1,21 @@
+//exploaring data types
+
+// Numbers
+let length = 172;
+
+// Strings
+let firstName = "Pranay";
+let lastName = "Patel";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object
+const Name = ["Pranay", "Neel", "Harsh"];
+
+// Date object:
+const date = new Date("2022-03-19");
